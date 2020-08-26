bd2904ce564e58261c72dccd065f7a2f39667a90
Taofeek <taofeekolamilekan218@gmail.com>
2020-08-26 14:19:07 +0100

    Spring boot welcome program

---
+++ src/main/java/com/taocoder/HelloSpring/HelloSpringApplication.java
@@ -0,0 +1,12 @@
+package com.taocoder.HelloSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringApplication.class, args);
+	}
+}
